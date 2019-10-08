520577f420a71a874f34cbc8ba483b43ea44f0f2
Payton Whipple <web-dev@paytonwhipple.com>
2019-10-08 12:08:53 -0600

    Initial Commit

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-  </div>
-);
+const App = () => {
+
+
+  return (
+    <BrowserRouter>
+
+    </BrowserRouter>
+  );
+
+};
 
 export default App;
